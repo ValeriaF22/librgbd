62e0f7c324fdc463ca1478ba62e9fc50be29b681
ValeriaF22 <v.filippou22@gmail.com>
2023-06-23 13:36:19 +0300

    Update opencv_imshow.py

--- examples/python/opencv_imshow.py
+++ examples/python/opencv_imshow.py
@@ -3,7 +3,7 @@ import argparse
 import examples_utils
 import numpy as np
 import cv2
-
+import os
 
 def main():
     parser = argparse.ArgumentParser(
@@ -13,12 +13,18 @@ def main():
     parser.add_argument("-i", "--input", help="path to input 3D video file")
 
     args = parser.parse_args()
-    if args.input is None:
-        print(
-            "No input file path specified. You can download a sample video from https://telegie.com/posts/A9oofdweNJ4"
-        )
-        return
+    # if args.input is None:
+    #     print(
+    #         "No input file path specified. You can download a sample video from https://telegie.com/posts/A9oofdweNJ4"
+    #     )
+    #     return
+    
+    # path of the mkv file
+    args.input = "cat.mkv"
 
+    # Specify the output directory path
+    output_dir = "/Users/user/Desktop/mkv_results/librgbd_results/0010_K_R"
+    
     # Parse the video file (record).
     record_parser = rgbd.RecordParser(args.input)
     record = record_parser.parse(True, True)
@@ -58,11 +64,46 @@ def main():
         # Improve visibility when shown via cv2.imshow.
         rgb_array = cv2.cvtColor(rgb_array, cv2.COLOR_BGR2RGB)
         depth_array = depth_array * 20
+        depth_map_path = os.path.join(output_dir, f"depth_map_{index}.png")
+
+        # Save depth map as an image using OpenCV.
+        cv2.imwrite(depth_map_path, depth_array)
+
+        print(f"Depth map {index} saved at {depth_map_path}")    
 
         cv2.imshow("color", rgb_array)
         cv2.imshow("depth", depth_array)
         cv2.waitKey(1)
 
+    # Define output video path and parameters
+    output_path = output_dir + "/rgb.mp4"
+    fps = 60.0  # Frames per second
+    width = record_tracks.color_track.width
+    height = record_tracks.color_track.height
+
+    # Define the codec for the output video
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+    # Create VideoWriter object
+    video_writer = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
+    # Write frames to the output video
+    for index in range(len(record_video_frames)):
+        yuv_frame = yuv_frames[index]
+        rgb_array = examples_utils.convert_yuv_frame_to_rgb_array(yuv_frame)
+
+        # Improve visibility when shown via cv2.imshow.
+        rgb_array = cv2.cvtColor(rgb_array, cv2.COLOR_BGR2RGB)
+
+        # Write the frame to the output video
+        video_writer.write(rgb_array)
+
+    # Release the VideoWriter object
+    video_writer.release()
+
+    print(f"Video saved at {output_path}")
 
 if __name__ == "__main__":
     main()
+
+
